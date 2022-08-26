253f8a52117361f131ac6d87ea000b64c105b1f9
jessedutton <jessedutton@gmail.com>
2022-08-26 13:58:08 -0600

    first commit

---
+++ awstok.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Copyright 2020 Jesse Dutton
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# The easiest way to use this is to add a funtion to your .bashrc as follows:
+#
+#   export AWS_MFA_ARN='arn:aws:iam::###:mfa/NAME'  # see first echo, below
+#   awstok() { eval "$(awstok.sh)" }
+#
+
+if [[ -z "$AWS_MFA_ARN" ]]; then
+  echo -e "First, set the environment variable AWS_MFA_ARN. After MFA is setup,\nthis value is shown on your user page in the AWS console. For hardware\ndevices, use the device serial number." >&2
+  exit 1
+fi
+if [[ -z "$(which aws)" ]]; then
+  echo "Dependency 'aws' is not in the path" >&2
+  exit 1
+fi
+if [[ -z "$(which jq)" ]]; then
+  echo "Dependency 'jq' is not in the path" >&2
+  exit 1
+fi
+
+# read -p writes to stderr
+read -p "Enter the value from Authenticator: " token
+if [[ -z "$token" ]]; then
+  echo "Aborted" >&2
+  exit 1
+fi
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+result="$(aws sts get-session-token --serial-number "$AWS_MFA_ARN" --token-code "$token")"
+if [ $? -ne 0 ]; then
+  echo "${result}" >&2
+  exit 1
+fi
+
+cat<<EOF
+export AWS_ACCESS_KEY_ID="$(echo "$result" | jq -r .Credentials.AccessKeyId)"
+export AWS_SECRET_ACCESS_KEY="$(echo "$result" | jq -r .Credentials.SecretAccessKey)"
+export AWS_SESSION_TOKEN="$(echo "$result" | jq -r .Credentials.SessionToken)"
+EOF
---
+++ readme.md
@@ -0,0 +1,22 @@
+# awstok
+This is a simple shell script that does one thing well. That one thing is ask
+for the MFA code from your authenticator app, and pass it to AWS. Maybe that's
+two things.
+
+## Use eval
+Because of how shell variables work, the commands that set environment
+variables needs to be executed from within the shell where the variables will
+live (i.e. not a subshell). The easiest way to do this is to put `awstok.sh`
+in your path, and add this to your `.bashrc` file:
+```
+export AWS_MFA_ARN='arn:aws:iam::###:mfa/NAME'
+awstok() { eval "$(awstok.sh)" }
+```
+The value for the `AWS_MFA_ARN` var for your user can be found on your user's
+AWS IAM page once you have setup MFA.
+
+# Dependencies
+Obviously, you will need the command line tool `aws` installed. We also depend
+on `jq`, which should exist as a package on most systems, so
+`apt-get install jq` or `brew install jq` as appropriate.
+
